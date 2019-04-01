5c37fa4ea5b84f1a91f57b6f348f5d968d341838
Juliet <chepngetichjuliet@gmail.com>
2019-04-01 10:34:45 +0300

    corrects the indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,125 +1,119 @@
-$(document).ready(function(){
-    $("#startBtn").click(function(){
-        $("#beginning").fadeOut(function(){
+$(document).ready(function () {
+    $("#startBtn").click(function () {
+        $("#beginning").fadeOut(function () {
             $("#hidden").fadeIn();
         });
     });
 
+    $("#btnSubmit").click(function() {
+        var question1 = document.quiz.question1.value;
+        var question2 = document.quiz.question2.value;
+        var question3 = document.quiz.question3.value;
+        var question4 = document.quiz.question4.value;
+        var question5 = document.quiz.question5.value;
+        var question6 = document.quiz.question6.value;
+        var question7 = document.quiz.question7.value;
+        var question8 = document.quiz.question8.value;
+        var correct = 0;
 
-
-
-
-
-
-
-
-    $("#startBtn").click(function(){
-        $("form#quiz").show();
-    });
-});
-    
-
-
-function checkSubmit() {
-
-    var question1 = document.quiz.question1.value; 
-    var question2 = document.quiz.question2.value; 
-    var question3 = document.quiz.question3.value; 
-    var question4 = document.quiz.question4.value; 
-    var question5 = document.quiz.question5.value; 
-    var question6 = document.quiz.question6.value; 
-    var question7 = document.quiz.question7.value;
-    var question8 = document.quiz.question8.value;
-    var correct = 0;
-
-    if (question1 == "Boolean") {
-        correct++;  
+        if (question1 == "Boolean") {
+            correct++;
         }
 
-    if (question2 == "Returns true if the argument is not a number otherwise it is false") {
-        correct++;
+        if (question2 == "Returns true if the argument is not a number otherwise it is false") {
+            correct++;
         }
 
-    if (question3 == "The head section") {
-        correct++;
+        if (question3 == "The head section") {
+            correct++;
         }
-     if (question4 == "For") {
-        correct++;
-        }
-
-    if (question5== "57") {
-        correct++;
+        if (question4 == "For") {
+            correct++;
         }
 
-    if (question6 == "No value or object") {
-        correct++;
+        if (question5 == "57") {
+            correct++;
         }
 
-    if (question7 == "alert") {
-        correct++;
+        if (question6 == "No value or object") {
+            correct++;
         }
 
-    if (question7 == "[") {
-        correct++;
-    }
+        if (question7 == "alert") {
+            correct++;
+        }
 
-    var markRange;
+        if (question8 == "[") {
+            correct++;
+        }
 
-    if (correct<1) {
-        marks= 3;
-    }
-    else {
-        marks=0;
-    }
-    if (correct>0 && correct <2) {
-        marks = 2;
-    }
-    else {
-        marks=0;
-    }
-    if (correct > 1 && correct < 3) {
-        marks = 2;
-    }
-    else {
-        marks=0;
-    }
-    if (correct > 2 && correct < 4) {
-        marks = 1;
-    }
-    else {
-        marks=0;
-    }
-    if (correct > 3) {
-        marks= 0
-    }
-    else {
-        marks=0;
-    }
-     
-     var percentage = correct/8 * 100
-    if(percentage >79){
-        message="Excellent work"
-    }else if (perceentage > 49) {
-        message="Fairly passed"
-    } else (percentage < 50) {
-        message="Poorly done .Retake test"
-    }
+        
+        var percentage = (correct / 7 )* 100
 
+        
+        if (percentage > 79) {
+            $("#hidden").fadeOut(function() {
+                $("#results").fadeIn(function () {
+                    $(".status").text(`Congratulations!`)
+                    $(".status2").text(`You have passed excellently!`)
+                    $(".show-score").text(`  Your score is: ${percentage}%`);
+                })
+            })
+           
 
+        } else if (percentage > 49) {
+            $("#hidden").fadeOut(function () {
+                $("#results").fadeIn(function () {
+                    $(".status").text(`Congratulations!`)
+                    $(".status2").text(`You have passed fairly!`)
+                    $(".show-score").text(`Your score is:  ${percentage}%`)
+                });
+            });
+        } else {
+            $("#hidden").fadeOut(function () {
+                $("#results").fadeIn(function () {
+                    $(".status").text(`You have have failed!`)
+                    $(".status2").text(`Retake the quiz when ready!`)
+                    $(".show-score").text(`  Your score is:${percentage}%`)
+                    $("#retake-btn ").fadeIn(function() {
+                        $("#retake-btn").click(function() {
+                            $("#results").fadeOut(function() {
+                                $("#hidden").fadeIn();
+                            })
+                        })
+                    });
+                })
+            })
+        }
 
-
-    document.getElementById("after_submit").style.visibility ="visible");
-    document.getElementById
-    
-
-   // document.getElementById("messages").innerHTML = messages[marks];
-   // document.getElementById("number_correct").innerHTML = "You got " + percentage + " % .";
-
-    $ ("form#quiz").hide();
-    event.preventDefault();
-
-
-    $("#btnSubmit").click(function(){
-        $("#hidden").fadeIn();
+        
+        
     })
-}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
